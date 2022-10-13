2f8da4267feb6778709187fd32ec5e862e4a31e5
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2022-10-13 15:27:39 -0300

    add redirect button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import { BackgroundCircles } from './BackgroundCircles'
@@ -21,7 +22,8 @@ export default function Hero({ }: Props) {
       <BackgroundCircles />
       <Image className="relative rounded-full h-32 w-32 mx-auto object-cover" src="" width={32}
         height={32} alt="" />
-      <div>
+
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Front-end web development
         </h2>
@@ -29,15 +31,24 @@ export default function Hero({ }: Props) {
           <span className="mr-3">{text}</span>
           <Cursor cursorColor='#F7AB0A' />
         </h1>
-        <div>
-          <button className="heroButton">About</button>
-          <button className="heroButton">Experience</button>
-          <button className="heroButton">Skills</button>
-          <button className="heroButton">Projects</button>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
         </div>
       </div>
 
 
-    </div>
+    </div >
   )
 }
